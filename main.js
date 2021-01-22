682f3615f6402f427763021902f04a4b0f20db25
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-22 21:42:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const readlineSync = require('readline-sync');
 
 entry=readlineSync.question("Give me number \n")
 while(entry>=0){
-    console.log(entry,"Go!");
+    console.log(entry);
     entry--
+    if (entry<0){
+      console.log("GO!")
+    }
     if (entry<0){
       entry=String(readlineSync.question("Give me a number \n"))
       console.log("Quit Program !")
